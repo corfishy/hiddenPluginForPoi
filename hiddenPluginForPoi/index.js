293cceab411972746c2f908402616e51b095ac66
corfishy <corfishy>
2015-08-18 10:30:23 +0800

    fix bug

--- hiddenPluginForPoi/index.js
+++ hiddenPluginForPoi/index.js
@@ -2,54 +2,54 @@ function hotkey()
 {
 if(event.ctrlKey)
 {
-	hideDisplay('needToHide')
+        hideDisplay('needToHide')
 }
 else if(event.shiftKey)
 {
-	if (imgCount==0)
-	{
-		imgCount = imgCount+1
-		coverByImage()
-	}
-	else
-	{
-		imgCount = imgCount-1
-		uncoverByImage()
-	}
+        if (imgCount==0)
+        {
+                imgCount = imgCount+1
+                coverByImage()
+        }
+        else
+        {
+                imgCount = imgCount-1
+                uncoverByImage()
+        }
 }
 }
 function hideDisplay(toHide)
 {
-	if (getElementToHide(toHide).style.display=="none")
-	{
-		getElementToHide(toHide).style.display=""
-	}
-	else
-	{
-		getElementToHide(toHide).style.display="none"
-	}
+        if (getElementToHide(toHide).style.display=="none")
+        {
+                getElementToHide(toHide).style.display=""
+        }
+        else
+        {
+                getElementToHide(toHide).style.display="none"
+        }
 }
 function getElementToHide(idOfHide)
 {
-	return document.getElementById(idOfHide)
+        return document.getElementById(idOfHide)
 }
 function coverByImage()
 {
-	var coverElements=document.createElement("img")
-	coverElements.setAttribute('src','plugins/hide/defaultCover.jpg')
-	coverElements.setAttribute('id','coverImage')
-	coverElements.style.position="absolute"
-	coverElements.style.top="0px"
-	coverElements.style.left="0px"
-	coverElements.style.zIndex="999"
-	var toInsert = document.getElementById("needToHide")
-	toInsert.appendChild(coverElements)
+        var coverElements=document.createElement("img")
+        coverElements.setAttribute('src','plugins/hiddenPluginForPoi/defaultCover.jpg')
+        coverElements.setAttribute('id','coverImage')
+        coverElements.style.position="absolute"
+        coverElements.style.top="0px"
+        coverElements.style.left="0px"
+        coverElements.style.zIndex="999"
+        var toInsert = document.getElementById("needToHide")
+        toInsert.appendChild(coverElements)
 }
 function uncoverByImage()
 {
-	var toUncoverImage=document.getElementById("coverImage")
-	var parentOfImage=document.getElementById("needToHide")
-	parentOfImage.removeChild(toUncoverImage)
+        var toUncoverImage=document.getElementById("coverImage")
+        var parentOfImage=document.getElementById("needToHide")
+        parentOfImage.removeChild(toUncoverImage)
 }
 
 
